d86829c9bf391b40dd80d7c4577a31925082eca8
sidashen <shashasi8023@163.com>
2020-02-21 15:04:47 +0800

    [shasha] feat: test 2 for find element and repeat count

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,24 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+      List<String> sameElementList = new ArrayList<>();
+      List<String> listMap = new ArrayList<String>();
+      for (String key : collection2.keySet()) {
+        listMap = collection2.get(key);
+      }
+      for (String element1 : collection1) {
+        for (String element2 : listMap) {
+          if (element1.equals(element2)) {
+            sameElementList.add(element1);
+          }
+        }
+      }
+    return sameElementList;
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,23 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+      Map<String, Integer> newMap = new HashMap<>();
+      for (String element : collection1) {
+        String mapElement = String.valueOf(element.charAt(0));
+        int count = newMap.containsKey(mapElement) ? newMap.get(mapElement) : 0;
+        if (element.length() != 1) {
+          count = Integer.parseInt(String.valueOf(element.charAt(2)));
+          newMap.put(mapElement, count);
+          break;
+        }
+        newMap.put(mapElement, count + 1);
+      }
+    return newMap;
     }
 }
